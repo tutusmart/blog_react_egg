a709d30057c41636056f3c002b61a1a33222fb58
涂威 <tuwei@qq.com>
2020-03-07 17:56:36 +0800

    配置文件

--- blog/config/apiUrl.js
+++ blog/config/apiUrl.js
@@ -1,4 +1,6 @@
-let ipUrl = 'http://127.0.0.1:7001/default/'
+// let ipUrl = 'http://127.0.0.1:7001/default/'
+let ipUrl = 'http://106.54.141.244:7001/default/'
+
 
 let servicePath = {
     getArticleList:ipUrl + 'getArticleList' ,  //  首页文章列表接口
