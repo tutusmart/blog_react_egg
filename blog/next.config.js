47fa82bfdf4482731065270578c01c16b1120971
涂威 <tuwei@qq.com>
2020-03-11 17:10:01 +0800

    配置文件修改

--- blog/next.config.js
+++ blog/next.config.js
@@ -3,5 +3,15 @@ const withCss = require('@zeit/next-css')
 if(typeof require !== 'undefined'){
     require.extensions['.css']=file=>{}
 }
+//更换目录 及文件大小处理
+module.exports = withCss({
+    distDir: 'build',
+    generateEtags: false,
+    onDemandEntries: {
+        // period (in ms) where the server will keep pages in the buffer
+        maxInactiveAge: 25 * 1000,
+        // number of pages that should be kept simultaneously without being disposed
+        pagesBufferLength: 2,
+    }
+})
 
-module.exports = withCss({})
